aa987763c3649317052a3a473336394cf6c49a6c
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-05 02:55:10 -0300

    Program now update child libraries
    by [aripmav1896@gmail.com] in [2011-10-02 20:50:50 (-03:00)]

--- Ak.All/Program.cs
+++ Ak.All/Program.cs
@@ -1,25 +1,137 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Ak.DataAccess.XML;
 
 namespace Ak.All
 {
-    // ReSharper disable InconsistentNaming
     ///<summary>
     ///</summary>
     public class Program
     {
-        static void Main()
+        private static IDictionary<String, String> projects;
+        private static String chosenProject;
+        private static IDictionary<String, String> currentLibs;
+
+        public static void Main()
         {
-            Console.Write("Akeon Library. Add the DLLs to your project.");
+            welcome();
 
-            var node = new Node(@"D:\Caroline\Handhara\MEAK\_A\00.xml");
+            setProjectOptions();
 
-            node[0][0].Value += "...";
+            setPath();
 
-            node.BackUpAndSave();
+            setLibs();
+
+            copyLibs();
 
             Console.Read();
         }
+
+
+
+        private static void welcome()
+        {
+            Console.WriteLine("AKEON LIBRARY");
+            Console.WriteLine("===================================");
+        }
+
+
+
+        private static void setProjectOptions()
+        {
+            projects = new Dictionary<String, String>
+                           {
+                               {"DFM", @"D:\Lucas\Akeon\03 - DFM\Source\Library"},
+                               {"Responde", @"D:\Lucas\Akeon\02 - Responde\Source\Library"},
+                               {"Stories", @"D:\Lucas\Akeon\06 - Stories\Source\Library"}
+                           };
+        }
+
+
+
+        private static void setPath()
+        {
+            while (wrongChoice())
+            {
+                showProjectOptions();
+
+                chosenProject = Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+
+        private static bool wrongChoice()
+        {
+            return chosenProject == null || 
+                   (!projects.Keys.Contains(chosenProject)
+                        && chosenProject != "All");
+        }
+
+        private static void showProjectOptions()
+        {
+            Console.Write("Set your project library path (");
+            Console.Write("All");
+
+            foreach (var project in projects)
+            {
+                Console.Write(@" / {0}", project.Key);
+            }
+
+            Console.WriteLine("): ");
+        }
+
+
+
+        private static void setLibs()
+        {
+            currentLibs = new Dictionary<String, String>();
+
+            foreach (var lib in getLibs())
+            {
+                var fileName = new FileInfo(lib).Name;
+
+                if (fileName.Contains("Ak.All")) continue;
+
+                currentLibs.Add(fileName, lib);
+            }
+        }
+
+        private static IEnumerable<String> getLibs()
+        {
+            var currentDirectoryPath = Directory.GetCurrentDirectory();
+
+            return Directory.GetFiles(currentDirectoryPath, "Ak.*");
+        }
+
+
+
+        private static void copyLibs()
+        {
+            if (chosenProject != "All")
+                copyLibs(projects[chosenProject]);
+            else
+                foreach (var project in projects)
+                {
+                    copyLibs(project.Value);
+                }
+        }
+
+        private static void copyLibs(string path)
+        {
+            foreach (var lib in currentLibs)
+            {
+                var fileName = lib.Key;
+                var original = lib.Value;
+
+                var libCopy = Path.Combine(path, "Ak", fileName);
+
+                File.Copy(original, libCopy, true);
+                Console.WriteLine("Copied: {0}", libCopy);
+            }
+
+            Console.WriteLine();
+        }
     }
-    // ReSharper restore InconsistentNaming
 }
