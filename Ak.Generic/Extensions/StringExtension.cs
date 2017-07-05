1981686ca4feea8cfd14bd7f226775b6c82b650c
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-05 02:59:07 -0300

    String Extension passed to Ak
    by [aripmav1896@gmail.com] in [2011-11-17 02:45:28 (-02:00)]

---
+++ Ak.Generic/Extensions/StringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ak.Generic.Extensions
+{
+    ///<summary>
+    ///</summary>
+    public static class StringExtension
+    {
+        ///<summary>
+        /// Puts the first letter in Upper and the rest in Lower
+        ///</summary>
+        public static String Capitalize(this String str)
+        {
+            return str == null
+                       ? null
+                       : str.First().ToString().ToUpper()
+                         + str.Substring(1).ToLower();
+        }
+
+        ///<summary>
+        /// String format with words instead of number (e.g. {data} instead {0})
+        ///</summary>
+        public static String Format(this String str, IDictionary<String, String> replaces)
+        {
+            return replaces.Aggregate(str, (current, replace) =>
+                        current.Replace("{{" + replace.Key + "}}", replace.Value));
+        }
+
+    }
+}
