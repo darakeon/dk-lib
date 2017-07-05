f368a5841cfadad09af9b4e16e068cb97cf84050
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-05 02:59:32 -0300

    Add Dictionary extension for sum or concat list items
    by [aripmav1896@gmail.com] in [2011-12-06 17:57:34 (-02:00)]

---
+++ Ak.Generic/Extensions/DictionaryExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ak.Generic.Extensions
+{
+    ///<summary>
+    ///</summary>
+    public static class DictionaryExtension
+    {
+        internal static void SumItem<TKey>(this IDictionary<TKey, Double> sumList, TKey key, Double value)
+        {
+            if (sumList.ContainsKey(key))
+                sumList[key] += value;
+            else
+                sumList.Add(key, value);
+        }
+    }
+}
