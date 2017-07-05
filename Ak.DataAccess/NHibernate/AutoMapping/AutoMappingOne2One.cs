0aa8a9a69ac0385ac11ebd8f8c3b09237b87ae29
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-05 03:11:39 -0300

    One to one helper on NH
    by [Dara Keon (Ísis)] in [2013-03-07 13:32:35 (-03:00)]

---
+++ Ak.DataAccess/NHibernate/AutoMapping/AutoMappingOne2One.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+using Ak.Generic.Reflection;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Mapping;
+
+namespace DFM.Repositories.Mappings
+{
+    public static class AutoMappingOne2One
+    {
+        public static IdentityPart IsWeakEntity<T, TFather>(this AutoMapping<T> mapping, Expression<Func<T, TFather>> memberExpression)
+        {
+            var motherEntityName = Property.Name(memberExpression);
+
+            return mapping.Id()
+                .GeneratedBy.Foreign(motherEntityName);
+        }
+
+    }
+}
