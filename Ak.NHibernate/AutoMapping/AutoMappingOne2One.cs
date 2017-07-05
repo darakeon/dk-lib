6846808ad02accfa468dad5db2b41be396502128
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-05 03:17:50 -0300

    Fixed relation 1:1 in NH
    by [Dara Keon (Ísis)] in [2015-03-18 18:04:56 (-03:00)]

--- Ak.NHibernate/AutoMapping/AutoMappingOne2One.cs
+++ Ak.NHibernate/AutoMapping/AutoMappingOne2One.cs
@@ -6,24 +6,24 @@ using FluentNHibernate.Mapping;
 
 namespace Ak.NHibernate.AutoMapping
 {
-    ///<summary>
-    /// Extensions to relation 1:1
-    ///</summary>
-    public static class AutoMappingOne2One
-    {
-        ///<summary>
-        /// Used on weak entity of 1:1.
-        /// Use HasOne on the strong entity mapping.
-        ///</summary>
-        ///<param name="mapping">Object received in AutoMappingOverride</param>
-        ///<param name="memberExpression">Lambda of entity that is the wrong on relation</param>
-        public static IdentityPart IsWeakEntity<T, TFather>(this AutoMapping<T> mapping, Expression<Func<T, TFather>> memberExpression)
-        {
-            var motherEntityName = memberExpression.GetName();
+	///<summary>
+	/// Extensions to relation 1:1
+	///</summary>
+	public static class AutoMappingOne2One
+	{
+		/// <summary>
+		///  Used on weak entity of 1:1.
+		///  Use HasOne on the strong entity mapping.
+		/// </summary>
+		/// <param name="mapping">Object received in AutoMappingOverride</param>
+		/// <param name="propertyExpression">Lambda of property with correspondent entity</param>
+		/// <param name="parentExpression">Lambda of property that represents this entity in other entity</param>
+		public static OneToOnePart<TFather> IsWeakEntity<T, TFather>(this AutoMapping<T> mapping, Expression<Func<T, TFather>> propertyExpression, Expression<Func<TFather, T>> parentExpression)
+		{
+			var body = (MemberExpression)parentExpression.Body;
 
-            return mapping.Id()
-                .GeneratedBy.Foreign(motherEntityName);
-        }
+			return mapping.HasOne(propertyExpression).PropertyRef(body.Member.Name);
+		}
 
-    }
+	}
 }
