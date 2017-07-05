9bbd585834a9b5c585771df5a02369fb9f416dba
lucas_carol <lc.sales.duarte@gmail.com>
2017-07-05 03:10:02 -0300

    Checkbox for
    by [aripmav1896@gmail.com] in [2012-03-17 22:00:26 (-03:00)]

---
+++ Ak.MVC/Forms/HtmlHelperExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Ak.MVC.Forms
+{
+    ///<summary>
+    /// 
+    ///</summary>
+    public static class HtmlHelperExtension
+    {
+        ///<summary>
+        /// Create a checkbox
+        ///</summary>
+        ///<param name="htmlHelper"></param>
+        ///<param name="expression">The list which will receive the values in controller</param>
+        ///<param name="value">The value of this checkbox</param>
+        ///<param name="htmlAttributes">Optional</param>
+        ///<typeparam name="TModel">The current Model</typeparam>
+        ///<typeparam name="TProperty">The property that will receive the value in Controller</typeparam>
+        public static MvcHtmlString CheckBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, object value, object htmlAttributes)
+        {
+            var model = htmlHelper.RadioButtonFor(expression, value, htmlAttributes);
+
+            var checkbox = model.ToString().Replace(@"type=""radio""", @"type=""checkbox""");
+
+            return new MvcHtmlString(checkbox);
+        }
+
+
+        ///<summary>
+        /// Create a checkbox
+        ///</summary>
+        ///<param name="htmlHelper"></param>
+        ///<param name="expression">The list which will receive the values in controller</param>
+        ///<param name="value">The value of this checkbox</param>
+        ///<typeparam name="TModel">The current Model</typeparam>
+        ///<typeparam name="TProperty">The property that will receive the value in Controller</typeparam>
+        public static MvcHtmlString CheckBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, object value)
+        {
+            return htmlHelper.CheckBoxFor(expression, value, null);
+        }
+
+    }
+}
