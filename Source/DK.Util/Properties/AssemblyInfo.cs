ab1d74a605cc3e8798967f862032d924b4c73048
Dara Keon <lc.sales.duarte@gmail.com>
2019-05-21 17:42:46 -0300

    util: rename generic project 2/2

--- Source/DK.Util/Properties/AssemblyInfo.cs
+++ Source/DK.Util/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DK.Generic")]
+[assembly: AssemblyTitle("DK.Util")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DK.Generic")]
+[assembly: AssemblyProduct("DK.Util")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
