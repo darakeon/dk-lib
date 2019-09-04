8f07eea62716d7b954c72eeaf473d7fa8490943e
Dara Keon <lc.sales.duarte@gmail.com>
2019-09-03 22:43:16 -0300

    publish: remove project, it's on Nuget now

--- Source/DK.All/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Keon Copier")]
-[assembly: AssemblyDescription("Copies the dll for other projects")]
-[assembly: AssemblyProduct("Keon Copier")]
-[assembly: Guid("4f7e8722-d4d2-4d59-aba1-cf4c1bcf1182")]
