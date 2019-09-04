8ffeec05037d9a617e65a164e5866c85d6c8c911
Dara Keon <lc.sales.duarte@gmail.com>
2019-09-03 22:39:33 -0300

    mvc: refactor authentication

--- Source/DK.MVC/Cookies/BrowserId.cs
+++ Source/DK.MVC/Cookies/BrowserId.cs
@@ -16,13 +16,13 @@ namespace Keon.MVC.Cookies
 		/// </summary>
 		public static String Get()
 		{
-			return Get(null);
+			return Get(false);
 		}
 
 		/// <summary>
 		/// Get (if needed, create) ticket
 		/// </summary>
-		public static String Get(Boolean? remember)
+		public static String Get(Boolean remember)
 		{
 			if (context == null)
 				throw new DKException("No http context");
@@ -31,9 +31,9 @@ namespace Keon.MVC.Cookies
 			{
 				add(Token.New(), remember);
 			}
-			else if (remember.HasValue)
+			else if (remember)
 			{
-				setCookie(get(), remember.Value);
+				setCookie(get());
 			}
 
 			return get();
@@ -55,26 +55,16 @@ namespace Keon.MVC.Cookies
 
 		private static String get()
 		{
-			if (session != null)
-			{
-				var value = session[name];
+			var value = session?[name];
 
-				if (value != null)
-					return value.ToString();
-			}
+			if (value != null)
+				return value.ToString();
 
 			var cookie = getCookies[name] ?? setCookies[name];
-			var cookieText = cookie?.Value ?? String.Empty;
-
-			if (!cookieText.EndsWith("_1"))
-				return null;
-
-			var ticket = cookieText.Substring(0, cookieText.Length - 2);
-
-			return ticket;
+			return cookie?.Value ?? String.Empty;
 		}
 
-		private static void add(String value, Boolean? remember)
+		private static void add(String value, Boolean remember)
 		{
 			if (session != null)
 			{
@@ -82,15 +72,15 @@ namespace Keon.MVC.Cookies
 				session.Add(name, value);
 			}
 
-			if (remember.HasValue)
+			if (remember)
 			{
-				setCookie(value, remember.Value);
+				setCookie(value);
 			}
 		}
 
-		private static void setCookie(String value, Boolean remember)
+		private static void setCookie(String value)
 		{
-			var cookie = new HttpCookie(name, value + "_" + (remember ? 1 : 0));
+			var cookie = new HttpCookie(name, value);
 
 			getCookies.Add(cookie);
 			setCookies.Add(cookie);
