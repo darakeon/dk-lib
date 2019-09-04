37e17ef6cb70838050618cef7727de83d4f0dff8
Dara Keon <lc.sales.duarte@gmail.com>
2019-09-03 22:44:53 -0300

    excel: remove unused project

--- Source/DK.Excel/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Keon Excel")]
-[assembly: AssemblyDescription("Excel pre-2007 reader")]
-[assembly: AssemblyProduct("Keon Excel")]
-[assembly: Guid("36ee4178-82d5-4f9a-89de-bf39be696244")]
