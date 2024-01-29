5dd8091c499871e3e09c66353fd5f34371431487
Dara Keon <lc.sales.duarte@gmail.com>
2024-01-29 01:58:46 +0000

    ci: change version on dependabot branches

---
+++ .github/change-version.py
@@ -0,0 +1,53 @@
+from os import system
+from subprocess import PIPE, run
+from re import search, sub
+from sys import argv
+
+
+def terminal(command):
+    process = run(command.split(' '), stdout=PIPE)
+    return process.stdout.decode('utf-8').split('\n')
+
+
+commits = terminal('git log origin/main.. --pretty=format:%s')
+diff_files = terminal('git diff origin/main --stat')
+
+commit_acronyms = {
+    'Eml': 'eml',
+    'MVC': 'mvc',
+    'NHibernate': 'nh',
+    'TwoFactorAuth': 'tfa',
+    'Util': 'util',
+    'XML': 'xml',
+}
+
+for diff_file in diff_files:
+    if '.csproj' not in diff_file:
+        continue
+
+    file_path = diff_file.split(' ')[1]
+    project = file_path.split('/')[1]
+    acronym = commit_acronyms[project]
+
+    commit_message = f'{acronym}: update version'
+
+    if commit_message in commits:
+        print(f'Already committed for project {project}')
+
+    else:
+        with open(file_path) as file:
+            content = file.read()
+
+        pattern = r'(<Version>\d+\.\d+\.)(\d+)(</Version>)'
+        version = search(pattern, content)
+
+        current = int(version.group(2))
+        new = current + 1
+        content = sub(pattern, rf'\g<1>{new}\g<3>', content)
+
+        with open(file_path, 'w') as file:
+            file.write(content)
+
+        system(f'git add {file_path}')
+        system(f'git commit -m "{commit_message}"')
+        system(f'git push')
